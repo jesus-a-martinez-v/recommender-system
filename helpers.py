3ca2379eb28f7f7a181eeed846a5265e212c7bea
Jesús Martínez <jesus.a.martinez.v@gmail.com>
2018-10-12 04:03:55 +0000

    Finishes second notebook.

--- helpers.py
+++ helpers.py
@@ -121,7 +121,7 @@ def write_movies(handler, output_path='generated/wp_movies.ndjson'):
             fout.write(f'{json.dumps(movie)}\n')
 
 
-def get_movie_embedding_model(top_links, movie_to_index, embedding_size=30):
+def get_movie_embedding_model(top_links, movie_to_index, embedding_size=50):
     link = Input(name='link', shape=(1,))
     movie = Input(name='movie', shape=(1,))
 
@@ -175,159 +175,12 @@ def similar_movies(movie, movies, normalized_movies, movie_to_index, top_n=10):
         movie_title = movies[c][0]
         distance = distances[c]
         print(c, movie_title, distance)
-
-def gross(movie):
-    v = movie[1].get('gross')
-
-    if not v or ' ' not in v:
-        return None
-
-    v, unit = v.split(' ', 1)
-    unit = unit.lower()
-
-    if unit not in {'million', 'billion'}:
-        return None
-
-    if not v.startswith('$'):
-        return None
-
-    try:
-        v = float(v[1:])
-    except ValueError:
-        return None
-
-    if unit == 'billion':
-        v *= 1000
-
-    return v
-
-
-if __name__ == '__main__':
-    # dumps = get_wikipedia_dumps()
-    # print(dumps)
-    # path = download_dump(dumps)
-    # parser, handler = create_wiki_xml_parser_and_handler()
-    # parse_dumps(parser, path)
-    # write_movies(handler)
-
-    with open('data/wp_movies_10k.ndjson') as fin:
-        movies = [json.loads(line) for line in fin]
-
-    link_counts = Counter()
-
-    for movie in movies:
-        movie_links = movie[2]
-        link_counts.update(movie_links)
-
-    print(link_counts.most_common(10))
-
-    top_links = [link for link, count in link_counts.items() if count >= 3]
-    link_to_index = {link: index for index, link in enumerate(top_links)}
-    movie_to_index = {movie[0]: index for index, movie in enumerate(movies)}
-
-    pairs = []
-    for movie in movies:
-        movie_title = movie[0]
-        movie_links = movie[2]
-        pairs.extend((link_to_index[link], movie_to_index[movie_title])
-                     for link in movie_links
-                     if link in link_to_index)
-
-    pairs_set = set(pairs)
-    print(len(pairs))
-    print(len(top_links))
-    print(len(movie_to_index))
-
-    model = get_movie_embedding_model(top_links, movie_to_index)
-    model.summary()
-
-    random.seed(5)
-
-    positive_samples_per_batch = 512
-
-    model.fit_generator(batchifier(pairs, pairs_set, top_links, movie_to_index, positive_samples_per_batch),
-                        epochs=15,
-                        steps_per_epoch=len(pairs) // positive_samples_per_batch)
-
-    movie = model.get_layer('movie_embedding')
-    movie_weights = movie.get_weights()[0]
-    movie_lengths = np.linalg.norm(movie_weights, axis=1)
-    normalized_movies = (movie_weights.T / movie_lengths).T
-
-    similar_movies('Rogue One', movies, normalized_movies, movie_to_index)
-
-    link = model.get_layer('link_embedding')
-    link_weights = link.get_weights()[0]
-    link_lengths = np.linalg.norm(link_weights, axis=1)
-    normalized_links = (link_weights.T / link_lengths).T
-
-
-    def similar_links(link, top_links, normalized_links, link_to_index, top_n=10):
-        distances = np.dot(normalized_links, normalized_links[link_to_index[link]])
-        closest = np.argsort(distances)[-top_n:]
-
-        for l in reversed(closest):
-            distance = distances[l]
-            print(l, top_links[l], distance)
-
-
-    similar_links('George Lucas', top_links, normalized_links, link_to_index)
-
-    best = ['Star Wars: The Force Awakens', 'The Martian (film)', 'Tangerine (film)', 'Straight Outta Compton (film)',
-            'Brooklyn (film)', 'Carol (film)', 'Spotlight (film)']
-    worst = ['American Ultra', 'The Cobbler (2014 film)', 'Entourage (film)', 'Fantastic Four (2015 film)',
-             'Get Hard', 'Hot Pursuit (2015 film)', 'Mortdecai (film)', 'Serena (2014 film)', 'Vacation (2015 film)']
-
-    y = np.asarray(([1] * len(best)) + ([0] * len(worst)))
-    X = np.asarray([normalized_movies[movie_to_index[movie]]
-                    for movie in (best + worst)])
-
-    print(X.shape)
-
-    classifier = svm.SVC(kernel='linear')
-    classifier.fit(X, y)
-
-    estimated_movie_ratings = classifier.decision_function(normalized_movies)
-    best = np.argsort(estimated_movie_ratings)
-    print('Best: ')
-    for c in reversed(best[-5:]):
-        print(c, movies[c][0], estimated_movie_ratings[c])
-
-    print('Worst: ')
-    for c in best[:5]:
-        print(c, movies[c][0], estimated_movie_ratings[c])
-
-    rotten_y = np.asarray([float(movie[-2][:-1]) / 100 for movie in movies if movie[-2]])
-    rotten_X = np.asarray([normalized_movies[movie_to_index[movie[0]]] for movie in movies if movie[-2]])
-
-    TRAINING_CUT_OFF = int(len(rotten_X) * 0.8)
-    regressor = LinearRegression()
-    regressor.fit(rotten_X[:TRAINING_CUT_OFF], rotten_y[:TRAINING_CUT_OFF])
-
-    error = (regressor.predict(rotten_X[TRAINING_CUT_OFF:]) - rotten_y[TRAINING_CUT_OFF:])
-    print(f'Mean Squared Error: {np.mean(error ** 2)}')
-
-    error = (np.mean(rotten_y[:TRAINING_CUT_OFF]) - rotten_y[TRAINING_CUT_OFF:])
-    print(f'Mean Squared Error: {np.mean(error ** 2)}')
-
-
-    movie_gross = [gross(movie) for movie in movies]
-    movie_gross = np.asarray([g for g in movie_gross if g is not None])
-
-    highest = np.argsort(movie_gross)[-10:]
-
-    for c in reversed(highest):
-        print(c, movies[c][0], movie_gross[c])
-
-    gross_y = np.asarray([g for g in movie_gross if g])
-    gross_X = np.asarray([normalized_movies[movie_to_index[movie[0]]] for movie, g in zip(movies, movie_gross) if g])
-
-    TRAINING_CUT_OFF = int(len(gross_X) * 0.8)
-    r = LinearRegression()
-    r.fit(gross_X[:TRAINING_CUT_OFF], gross_y[:TRAINING_CUT_OFF])
-
-    error = (r.predict(gross_X[TRAINING_CUT_OFF:]) - gross_y[TRAINING_CUT_OFF:])
-    print(f'Mean Squared Error: {np.mean(error ** 2)}')
-
-    error = (np.mean(gross_y[:TRAINING_CUT_OFF]) - gross_y[TRAINING_CUT_OFF:])
-    print(f'Mean Squared Error: {np.mean(error ** 2)}')
+        
+def similar_links(link, top_links, normalized_links, link_to_index, top_n=10):
+    distances = np.dot(normalized_links, normalized_links[link_to_index[link]])
+    closest = np.argsort(distances)[-top_n:]
+    
+    for l in reversed(closest):
+        distance = distances[l]
+        print(l, top_links[l], distance)
+        
